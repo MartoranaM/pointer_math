2fa186166f03c7cf1ed305a4e0449c13438eee89
Martoranam <92488268+MartoranaM@users.noreply.github.com>
2022-09-06 10:53:39 -0500

    Handles vector input

--- pointer_math.cpp
+++ pointer_math.cpp
@@ -3,13 +3,17 @@
 
 using namespace std;
 
-int* apply_all(const int *const array1, size_t size1, const int *const array2, size_t size2);
-void print (const int *const array, size_t size);
+int* apply_all(const int* const array1, size_t size1, const int* const array2, size_t size2); //Creates an int* array to store the multiplied values of 2 arrays
+void print (const int* const array, size_t size);
 
 int main() {
     const size_t array1_size {5};
     const size_t array2_size {3};
-    
+
+    vector <int> vec {10,20,30,40,50};
+    size_t vec_size = vec.size();
+
+    int* arr_pointer = vec.data();
     int array1[] {1,2,3,4,5};
     int array2[] {10,20,30};
     
@@ -28,17 +32,23 @@ int main() {
     delete [] results;
     cout << endl;
 
+    cout << arr_pointer;
+    cout << "Pointer Math, with a vector input: \n";
+    int *result2 = apply_all(arr_pointer,vec_size,array2, array2_size);
+    const size_t result2_size {vec_size * array2_size};
+    print (result2, result2_size);
+
     return 0;
 }
 
-void print (const int *const array, size_t size) {
+void print (const int* const array, size_t size) {
     cout << "[";
     for (size_t i{0}; i<size; i++)
         cout << array[i] << " ";
     cout << "]";
 }
 
-int* apply_all(const int *const array1, size_t size1, const int *const array2, size_t size2) {
+int* apply_all(const int* const array1, size_t size1, const int* const array2, size_t size2) {
     int *new_array{};
     new_array = new int[size1 * size2];
     int position{0};
